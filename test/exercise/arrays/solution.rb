76e211330362839c97b285d7e14fbfda38d986de
Elza.Sharafutdinova <elza.shar@gmail.com>
2021-04-18 11:55:25 +0400

    Сompleted task on arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,25 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        sort_array = array.sort
+        y = sort_array.last
+        array.map { |el| el.positive? ? y : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        max_index = array.size - 1
+        mid_index = max_index / 2
+        if array.empty? || array.first > query || array.last < query
+          -1
+        elsif array[mid_index] == query
+          mid_index
+        elsif query < array[mid_index]
+          search(array[0, mid_index], query)
+        elsif query > array[mid_index]
+          new_array = array.slice(mid_index + 1, (array.size / 2))
+          result = search(new_array, query)
+          result == -1 ? -1 : mid_index + 1 + result
+        end
       end
     end
   end
