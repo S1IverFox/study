3a2cf9ea33df0699958cfc8e3a79dc09ca0a952c
Elza.Sharafutdinova <elza.shar@gmail.com>
2021-04-26 17:55:02 +0400

    Edited solution fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,12 +5,15 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        filtered_array = array.select { |film| film['country'].to_s.split(',').length >= 2 && film['rating_kinopoisk'].to_f.positive? }
-        filtered_array.map { |film| film['rating_kinopoisk'].to_f }.reduce(:+) / filtered_array.length
+        a = array.reject { |film| film['country'].nil? || film['rating_kinopoisk'].nil? }
+        filtered_array = a.select { |film| film['country'].to_s.split(',').length >= 2 && film['rating_kinopoisk'].to_f.positive? }
+        sum_of_rating = filtered_array.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f }
+        sum_of_rating / filtered_array.length
       end
 
       def chars_count(films, threshold)
-        filtered_array = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+        a = films.reject { |film| film['rating_kinopoisk'].nil? }
+        filtered_array = a.select { |film| film['rating_kinopoisk'].to_f >= threshold }
         filtered_array.reduce(0) { |sum, film| sum + film['name'].count('и') }
       end
     end
